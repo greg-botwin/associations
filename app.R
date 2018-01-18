cf3527ef51813b60c4814ea3a31ca50b299e3fb6
Greg Botwin <gbotwin@gmail.com>
2018-01-18 08:50:11 -0800

    options to download tab tables closes #11

--- app.R
+++ app.R
@@ -95,13 +95,17 @@ ui <- fluidPage(
     # Show a table
     mainPanel(
       tabsetPanel(
-        tabPanel("By Gene Search", DT::dataTableOutput("table_genes")),
-        tabPanel("By Gene-Pheno Search", DT::dataTableOutput("table_gene_snps")),
-        tabPanel("By Gene-Pheno-SNP Search", DT::dataTableOutput("table_snps"))
+        tabPanel("By Gene Search", 
+                 DT::dataTableOutput("table_gene"),
+                 downloadButton("download_table_gene", "Download")),
+        tabPanel("By Gene-Pheno Search", DT::dataTableOutput("table_gene_pheno"),
+                 downloadButton("download_table_gene_pheno", "Download")),
+        tabPanel("By Gene-Pheno-SNP Search", DT::dataTableOutput("table_gene_pheno_snp"),
+                 downloadButton("download_table_gene_pheno_snp", "Download")))
       )
     )
   )
-)
+
 
   
 
@@ -139,7 +143,7 @@ server <- function(input, output, session) {
     
   
   # table_genes ----------------------------------------------------------------
-  output$table_genes<- DT::renderDataTable({
+  output$table_gene<- DT::renderDataTable({
     
     associations_filtered() %>%
       group_by(population, gene) %>%
@@ -152,9 +156,8 @@ server <- function(input, output, session) {
     
   })
   
-  # table_gene_snps ------------------------------------------------------------
-  output$table_gene_snps<- DT::renderDataTable({
-
+  # table_gene_pheno------------------------------------------------------------
+  output$table_gene_pheno<- DT::renderDataTable({
     associations_filtered() %>%
       group_by(population, gene, PHENOTYPE) %>%
       summarise(n_sig_snps = length(unique(RSID)),
@@ -166,9 +169,9 @@ server <- function(input, output, session) {
     
   })
   
-  # table_snps ----------------------------------------------------------------
+  # table_gene_pheno_snp---------------------------------------------------------
   # talk with talin about rs2066844, non unique SNP
-  output$table_snps <- DT::renderDataTable({
+  output$table_gene_pheno_snp <- DT::renderDataTable({
     associations_filtered() %>%
       group_by(population, gene, PHENOTYPE, RSID, SNP) %>%
       summarise(p_value = P,
@@ -185,6 +188,58 @@ server <- function(input, output, session) {
   updateSelectizeInput(session, 'genelist', choices = unique_genes$gene, server = TRUE)
   updateSelectizeInput(session, 'snplist', choices = unique_rsids$RSID, server = TRUE)
   
+  # Downloadable csv of selected dataset ---------------------------------------
+  output$download_table_gene <- downloadHandler(
+    filename = function() {
+      paste(Sys.Date(), "associations-table-gene", ".csv", sep = "-")
+    },
+    content = function(file) {
+      
+      write.csv(associations_filtered() %>%
+                  group_by(population, gene) %>%
+                  summarise(n_phenos = length(unique(PHENOTYPE)),
+                            phenos = paste(unique(PHENOTYPE), collapse = ", "),
+                            min_p = min(P),
+                            max_OR = max(OR),
+                            min_OR = min(OR)) %>%
+                  arrange(desc(n_phenos)), file, row.names = FALSE)
+    }
+  )
+  
+  output$download_table_gene_pheno <- downloadHandler(
+    filename = function() {
+      paste(Sys.Date(), "associations-table-gene-pheno", ".csv", sep = "-")
+    },
+    content = function(file) {
+      
+      write.csv(associations_filtered() %>%
+                  group_by(population, gene, PHENOTYPE) %>%
+                  summarise(n_sig_snps = length(unique(RSID)),
+                            SNPs = paste(unique(RSID), collapse = ", "),
+                            min_p = min(P),
+                            max_OR = max(OR),
+                            min_OR = min(OR)) %>%
+                  arrange(desc(n_sig_snps)), file, row.names = FALSE)
+    }
+  )
+  
+  output$download_table_gene_pheno_snp <- downloadHandler(
+    filename = function() {
+      paste(Sys.Date(), "associations-table-gene-pheno-snp", ".csv", sep = "-")
+    },
+    content = function(file) {
+      
+      write.csv(associations_filtered() %>%
+                  group_by(population, gene, PHENOTYPE, RSID, SNP) %>%
+                  summarise(p_value = P,
+                            chromosome = CHR,
+                            a1 = A1,
+                            n_miss = NMISS, 
+                            odds_ratio = OR,
+                            basse_pair = BP,
+                            snp_location = snp_location), file, row.names = FALSE)
+    }
+  )
 }
 
 # Run the application 
